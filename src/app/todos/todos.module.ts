43913f39246c2f6fa4a413c1a8043da2a8433460
Jose <alvarezriosjose@gmail.com>
2021-04-21 22:20:32 +0200

    ng new app

---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodosComponent } from './todos/todos.component';
+import { TodoComponent } from './todos/todo/todo.component';
+
+
+
+@NgModule({
+  declarations: [
+    TodosComponent,
+    TodoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodosModule { }
